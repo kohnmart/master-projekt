62fe017d9aa4f7ede92975c40879a644860332cd
mkohnle <martin.kohnle1@hs-augsburg.de>
2024-04-12 14:32:47 +0200

    update

--- config/path.py
+++ config/path.py
@@ -1,32 +1,36 @@
 import os
 
 
-class PathHelper:
+def get_project_root():
+    """Return the root directory of the project."""
+    return os.path.dirname(os.path.dirname(__file__))
 
-    """Helper class for path operations."""
-    
-    def __init__(self):
-        pass
-    
-    @staticmethod
-    def list_files(directory, extension='*'):
-        """List files in a directory with a specific extension."""
-        return directory.files(extension)
-    
-    @staticmethod
-    def join_paths(*paths):
-        """Join multiple paths together."""
-        return os.path.join(*paths)
-    
-    @staticmethod 
-    def get_full_cloth_type_path(sample_name):
-        type = sample_name.split("_")[0]
-        root = os.path.dirname(os.path.dirname(__file__))
-        return os.path.join(root, './dataset/train/', type, sample_name)
-        
-    @staticmethod    
-    def get_plot_path(subfolder_name):
-        root = os.path.dirname(os.path.dirname(__file__))
-        fullpath = os.path.join(root, 'notebooks/plots/', subfolder_name)     
-        os.makedirs(fullpath, exist_ok=True)
-        return fullpath    
+def get_models_path():
+    """Return the path to the directory containing models."""
+    return os.path.join(get_project_root(), 'src/models_export/multi_models/pytorch')
+
+def get_processed_images_path(version=None):
+    """Return the path to the directory containing processed images."""
+    processed_path = os.path.join(get_project_root(), "dataset/images/evaluate/processed")
+    if version:
+        processed_path = os.path.join(processed_path, version)
+    return processed_path
+
+def get_evaluation_path():
+    """Return the path to the evaluation directory."""
+    return os.path.join(get_project_root(), 'src/evaluation')
+
+def get_training_data_path(file_name=None):
+    """Return the path to the directory containing training data."""
+    train_path = os.path.join(get_project_root(), "dataset/train")
+
+    folder_name = file_name.split("_")[0]
+
+    if file_name:
+        train_path = os.path.join(train_path, folder_name, file_name)
+    return train_path
+
+def get_stable_input_path():
+    """Return the path to the directory containing stable diffusion input images."""
+    return os.path.join(get_project_root(), "dataset/images/stable-diffusion/input")
+
